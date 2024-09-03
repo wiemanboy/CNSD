54f3b4255e64e671b32ac289d64a35dc5206862e
wiemanboy <wiemanboy@gmail.com>
2024-09-03 13:36:45 +0200

    ADD: simple validation

--- src/main/java/com/wiemanboy/cnsdbankapplication/presentation/dto/request/BankAccountUpdateDTO.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/presentation/dto/request/BankAccountUpdateDTO.java
@@ -1,8 +1,10 @@
 package com.wiemanboy.cnsdbankapplication.presentation.dto.request;
 
 import com.wiemanboy.cnsdbankapplication.domain.enums.AccountStatus;
+import jakarta.validation.constraints.NotNull;
 
 public record BankAccountUpdateDTO(
+        @NotNull
         AccountStatus status
 ) {
 
--- src/main/java/com/wiemanboy/cnsdbankapplication/presentation/dto/request/CustomerCreateDTO.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/presentation/dto/request/CustomerCreateDTO.java
@@ -1,6 +1,11 @@
 package com.wiemanboy.cnsdbankapplication.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record CustomerCreateDTO(
+        @NotNull
+        @NotBlank
         String name
 ) {
 }
