11f5f94c37c01176707ddc619beae54278b85287
wiemanboy <wiemanboy@gmail.com>
2024-09-03 12:05:24 +0200

    ADD: Entity annotations

--- src/main/java/com/wiemanboy/cnsdbankapplication/domain/BankAccount.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/domain/BankAccount.java
@@ -1,18 +1,32 @@
 package com.wiemanboy.cnsdbankapplication.domain;
 
 import com.wiemanboy.cnsdbankapplication.domain.enums.AccountStatus;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * BankAccount has multiple customers associated with it, needs at least one customer to be created.
  * Also includes the value of the account and the status of the account.
  */
+@Entity
 public class BankAccount {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @ManyToMany
     private List<Customer> customers = new ArrayList<Customer>();
     private AccountStatus status = AccountStatus.OPEN;
     private int value = 0;
+
+    protected BankAccount() {
+    }
 
     public BankAccount(Customer customer) {
         addCustomer(customer);
--- src/main/java/com/wiemanboy/cnsdbankapplication/domain/Customer.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/domain/Customer.java
@@ -1,7 +1,26 @@
 package com.wiemanboy.cnsdbankapplication.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
     private String name;
+
+    @ManyToMany
+    private List<BankAccount> bankAccounts;
+
+    protected Customer() {
+    }
 
     public Customer(String name) {
         this.name = name;
