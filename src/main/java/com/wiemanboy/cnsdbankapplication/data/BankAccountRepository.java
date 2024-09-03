ebac074bfdff51004f3c91de441cf73491ec4466
wiemanboy <wiemanboy@gmail.com>
2024-09-03 12:06:28 +0200

    ADD: Repos

---
+++ src/main/java/com/wiemanboy/cnsdbankapplication/data/BankAccountRepository.java
@@ -0,0 +1,16 @@
+package com.wiemanboy.cnsdbankapplication.data;
+
+import com.wiemanboy.cnsdbankapplication.domain.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface BankAccountRepository extends JpaRepository<BankAccount, UUID> {
+    Optional<BankAccount> findBankAccountById(UUID id);
+
+    List<BankAccount> findBankAccounts();
+}
---
+++ src/main/java/com/wiemanboy/cnsdbankapplication/data/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.wiemanboy.cnsdbankapplication.data;
+
+import com.wiemanboy.cnsdbankapplication.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, UUID> {
+    Optional<Customer> findCustomerById(UUID id);
+
+    List<Customer> findCustomers();
+}
