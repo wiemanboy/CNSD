93470d1d8dda2ad881a1f63ea49abf254266a61f
wiemanboy <wiemanboy@gmail.com>
2024-09-03 11:28:14 +0200

    INITIAL COMMIT

---
+++ src/main/java/com/wiemanboy/cnsdbankapplication/CnsdBankApplication.java
@@ -0,0 +1,13 @@
+package com.wiemanboy.cnsdbankapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnsdBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnsdBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wiemanboy/cnsdbankapplication/CnsdBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wiemanboy.cnsdbankapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnsdBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
