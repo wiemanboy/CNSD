f19898074b070029b1acd8ddd397a2671fe8da34
wiemanboy <wiemanboy@gmail.com>
2024-09-10 10:55:56 +0200

    UPDATE: move to lombok for exception classes

--- src/main/java/com/wiemanboy/cnsdbankapplication/domain/exception/BankAccountNotFoundException.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/domain/exception/BankAccountNotFoundException.java
@@ -1,16 +1,15 @@
 package com.wiemanboy.cnsdbankapplication.domain.exception;
 
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
 public class BankAccountNotFoundException extends RuntimeException {
     private final UUID id;
 
     public BankAccountNotFoundException(UUID bankAccountId) {
         super(String.format("Bank account with id %s not found", bankAccountId));
         this.id = bankAccountId;
-    }
-
-    public UUID getId() {
-        return id;
     }
 }
--- src/main/java/com/wiemanboy/cnsdbankapplication/domain/exception/TooLittleCustomersInBankAccountException.java
+++ src/main/java/com/wiemanboy/cnsdbankapplication/domain/exception/TooLittleCustomersInBankAccountException.java
@@ -1,14 +1,13 @@
 package com.wiemanboy.cnsdbankapplication.domain.exception;
 
+import lombok.Getter;
+
+@Getter
 public class TooLittleCustomersInBankAccountException extends RuntimeException {
     private final int amount;
 
     public TooLittleCustomersInBankAccountException(int amount) {
         super(String.format("Bank account needs at least %s customers", amount));
         this.amount = amount;
-    }
-
-    public int getAmount() {
-        return amount;
     }
 }
