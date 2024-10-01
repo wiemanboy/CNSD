8a0265ec6df67e4a632247b9cdae5e0f7d6d2878
wiemanboy <wiemanboy@gmail.com>
2024-10-01 11:29:51 +0200

    ADD: page components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {AccountsComponent} from "./accounts/accounts.component";
+import {CustomersComponent} from "./customers/customers.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "login", component: LoginComponent},
+  {path: "", redirectTo: "/login", pathMatch: "full"},
+  {path: "accounts", component: AccountsComponent},
+  {path: "customers", component: CustomersComponent}
+];
