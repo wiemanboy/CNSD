58e565f765a7497afb9501134ed1bdff68fc48ab
wiemanboy <wiemanboy@gmail.com>
2024-10-15 15:23:40 +0200

    ADD: upload processed image to s3

--- consumer/src/main/java/com/wiemanboy/consumer/AwsConfig.java
+++ consumer/src/main/java/com/wiemanboy/consumer/AwsConfig.java
@@ -7,10 +7,11 @@ import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 
 @Configuration
-public class AwsSqsConfig {
+public class AwsConfig {
     @Value("${spring.cloud.aws.credentials.access-key}")
     private String accessKey;
     @Value("${spring.cloud.aws.credentials.secret-key}")
@@ -33,5 +34,15 @@ public class AwsSqsConfig {
     @Bean
     public SqsTemplate sqsTemplate(SqsAsyncClient sqsAsyncClient){
         return SqsTemplate.builder().sqsAsyncClient(sqsAsyncClient).build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client
+                .builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider
+                        .create(AwsSessionCredentials.create(accessKey, secretKey, sessionToken)))
+                .build();
     }
 }
--- consumer/src/main/java/com/wiemanboy/consumer/application/FileService.java
+++ consumer/src/main/java/com/wiemanboy/consumer/application/FileService.java
@@ -2,12 +2,44 @@ package com.wiemanboy.consumer.application;
 
 
 import org.springframework.stereotype.Service;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.core.sync.ResponseTransformer;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+
+import static java.lang.String.format;
 
 @Service
 public class FileService {
 
-    public void processImage(String fileKey){
+    private final S3Client s3Client;
+    private final String bucketName;
 
+    public FileService(S3Client s3Client) {
+        this.s3Client = s3Client;
+        this.bucketName = "jarno-messaging-bucket";
+    }
+
+    public void processImage(String fileKey){
         System.out.printf("Processing file %s%n", fileKey);
+
+        ResponseInputStream<GetObjectResponse> fileInputStream = s3Client.getObject(GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(format("uploads/%s", fileKey))
+                .build(), ResponseTransformer.toInputStream());
+
+        try {
+        s3Client.putObject(PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(format("processed/%s", fileKey))
+                .build(), RequestBody.fromInputStream(fileInputStream, fileInputStream.available()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
